35ff75c8c2b2e9c851555742bb623c36f95253bd
etemkocaaslan <120686301+etemkocaaslan@users.noreply.github.com>
2023-11-29 21:41:06 -0800

    815, 1727, 1685 have been solved

--- Test.cs
+++ Test.cs
@@ -412,7 +412,6 @@
 #endregion
 
 #region 1759
-
 //public class Solution
 //{
 //    public int CountHomogenous(string s)
@@ -438,7 +437,122 @@
 //        return (int)(count % mod);
 //    }
 //}
+#endregion
 
+#region 1685
+//public class Solution
+//{
+//    public static int[] GetSumAbsoluteDifferences(int[] nums)
+//    {
+//        int[] result = new int[nums.Length];
+
+//        int[] sumAbsoluteDifferences_left = new int[nums.Length];
+//        int previousValue = 0;
+//        for (int i = 0; i < nums.Length; i++)
+//        {
+//            if (i == 0)
+//                sumAbsoluteDifferences_left[i] = 0;
+//            else
+//                sumAbsoluteDifferences_left[i] = sumAbsoluteDifferences_left[i - 1] + Math.Abs(previousValue - nums[i]) * i;
+
+//            previousValue = nums[i];
+//        }
+
+
+//        int[] sumAbsoluteDifferences_right = new int[nums.Length];
+//        previousValue = 0;
+//        for (int i = nums.Length - 1; i >= 0; i--)
+//        {
+//            if (i == nums.Length - 1)
+//                sumAbsoluteDifferences_right[i] = 0;
+//            else
+//                sumAbsoluteDifferences_right[i] = sumAbsoluteDifferences_right[i + 1] + Math.Abs(previousValue - nums[i]) * (nums.Length - 1 - i);
+
+//            result[i] = sumAbsoluteDifferences_left[i] + sumAbsoluteDifferences_right[i];
+
+//            previousValue = nums[i];
+//        }
+
+//        return result;
+//    }
+//}
+#endregion
+
+#region 1727
+//public class Solution
+//{
+//    public int HammingWeight(uint n) => Convert.ToString(n, 2).Count((x) => x == '1');
+
+//}
+#endregion
+
+#region 815. Bus Routes
+//public class Solution
+//{
+//    public int NumBusesToDestination(int[][] routes, int source, int target)
+//    {
+//        if (source == target)
+//            return 0;
+
+//        Dictionary<int, List<int>> stopToBuses = new Dictionary<int, List<int>>();
+
+//        for (int i = 0; i < routes.Length; i++)
+//        {
+//            foreach (int stop in routes[i])
+//            {
+//                if (!stopToBuses.ContainsKey(stop))
+//                    stopToBuses[stop] = new List<int>();
+
+//                stopToBuses[stop].Add(i);
+//            }
+//        }
+
+//        Queue<int> stopsQueue = new Queue<int>();
+//        HashSet<int> exploredStops = new HashSet<int>();
+//        HashSet<int> exploredRoutes = new HashSet<int>();
+
+//        stopsQueue.Enqueue(source);
+//        exploredStops.Add(source);
+
+//        int numBuses = 0;
+
+//        while (stopsQueue.Count > 0)
+//        {
+//            int stopsToExplore = stopsQueue.Count;
+
+//            for (int i = 0; i < stopsToExplore; i++)
+//            {
+//                int currentExploringStop = stopsQueue.Dequeue();
+
+//                List<int> busesAtCurrentStop = stopToBuses.ContainsKey(currentExploringStop) ? stopToBuses[currentExploringStop] : new List<int>();
+
+//                foreach (int busAtCurrentStop in busesAtCurrentStop)
+//                {
+//                    if (exploredRoutes.Contains(busAtCurrentStop)) 
+//                        continue;
+
+//                    exploredRoutes.Add(busAtCurrentStop);
+
+//                    foreach (int nextExploringStop in routes[busAtCurrentStop])
+//                    {
+//                        if (exploredStops.Contains(nextExploringStop))
+//                            continue;
+
+//                        exploredStops.Add(nextExploringStop);
+
+//                        if (nextExploringStop == target)
+//                            return numBuses + 1;
+
+//                        stopsQueue.Enqueue(nextExploringStop);
+//                    }
+//                }
+//            }
+//            numBuses++;
+//        }   
+
+//        return -1;
+//    }
+//}
 #endregion
 public class Test
 {
