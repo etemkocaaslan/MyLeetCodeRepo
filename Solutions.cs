0a3b4a989a36e1b878f42e61cb0b58c6d0394a43
kasla <kasla@DESKTOP-SR937O1>
2023-12-08 01:20:58 -0800

    606 has been solved

--- Solutions.cs
+++ Solutions.cs
@@ -697,4 +697,37 @@ namespace leetcode
     //    public string LargestOddNumber(string num) => num[..(1 + num.LastIndexOfAny(new[] { '1', '3', '5', '7', '9' }))];
     //}
     #endregion
+
+    #region 606
+    //Definition for a binary tree node.
+    //public class TreeNode
+    //{
+    //    public int val;
+    //    public TreeNode left;
+    //    public TreeNode right;
+    //    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    //    {
+    //        this.val = val;
+    //        this.left = left;
+    //        this.right = right;
+    //    }
+    //}
+    //public class Solution
+    //{
+    //    public string Tree2str(TreeNode root)
+    //    {
+    //        if (root == null)
+    //            return "";
+
+    //        string result = root.val.ToString();
+
+    //        if (root.left != null || root.right != null)
+    //            result += "(" + Tree2str(root.left) + ")";
+
+    //        if (root.right != null)
+    //            result += "(" + Tree2str(root.right) + ")";
+    //        return result;
+    //    }
+    //}
+    #endregion
 }
