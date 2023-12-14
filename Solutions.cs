93c8fbb21686b637a41a2f08a67c8fe86c87afc0
kasla <kasla@DESKTOP-SR937O1>
2023-12-14 01:41:29 -0800

    2482 has been solved

--- Solutions.cs
+++ Solutions.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -843,7 +844,7 @@ namespace leetcode
     //            }
     //        }
 
-          
+
 
     //        int res = 0;
     //        for (int i = 0; i < mat.Length; i++)
@@ -857,6 +858,49 @@ namespace leetcode
     //        }
 
     //        return res;
+    //    }
+    //}
+    #endregion
+
+    #region 2482
+    //public class Solution
+    //{
+    //    public int[][] OnesMinusZeros(int[][] grid)
+    //    {
+    //        int m = grid.Length;
+    //        int n = grid[0].Length;
+    //        int[][] diff = new int[m][];
+
+    //        int[] onesRow = new int[m];
+    //        int[] zerosRow = new int[m];
+    //        int[] onesCol = new int[n];
+    //        int[] zerosCol = new int[n];
+
+    //        for (int i = 0; i < m; ++i)
+    //        {
+    //            diff[i] = new int[n];
+    //            for (int j = 0; j < n; ++j)
+    //            {
+    //                if (grid[i][j] != 1)
+    //                {
+    //                    zerosRow[i]++;
+    //                    zerosCol[j]++;
+    //                }
+    //                else
+    //                {
+    //                    onesRow[i]++;
+    //                    onesCol[j]++;
+    //                }
+    //            }
+    //        }
+    //        for (int i = 0; i < m; ++i)
+    //        {
+    //            for (int j = 0; j < n; ++j)
+    //            {
+    //                diff[i][j] = onesRow[i] + onesCol[j] - zerosRow[i] - zerosCol[j];
+    //            }
+    //        }
+    //        return diff;
     //    }
     //}
     #endregion
