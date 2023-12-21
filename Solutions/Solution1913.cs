e392f42b3d2740d1ffd570e50f4d12a04e06ea5e
kasla <kasla@DESKTOP-SR937O1>
2023-12-20 17:41:47 -0800

    Separated Solutions {242, 2353, 1637, 1436, 1913}

---
+++ Solutions/Solution1913.cs
@@ -0,0 +1,12 @@
+﻿namespace leetcode.Solutions
+{
+    public class Solution1913
+    {
+        public int MaxProductDifference(int[] nums)
+        {
+            var a = nums.ToList<int>();
+            a.Sort();
+            return (a[^2] * a[^1]) - (a[0] * a[1]);
+        }
+    }
+}
