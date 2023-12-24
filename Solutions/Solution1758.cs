62114f4ba0e23cbc32ce75a691aeb45494755a36
kasla <kasla@DESKTOP-SR937O1>
2023-12-24 02:05:43 -0800

    1758

---
+++ Solutions/Solution1758.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcode.Solutions
+{
+    public class Solution1758
+    {
+        public int MinOperations(string s)
+        {
+            int ones = 0;
+            int zeros = 0;
+            for (int i = 0; i < s.Length; ++i)
+            {
+                switch (i % 2)
+                {
+                    case 0:
+                        switch (s[i])
+                        {
+                            case '1':
+                                zeros++;
+                                break;
+                            case '0':
+                                ones++;
+                                break;
+
+                        }
+                        break;
+                    case 1:
+                        switch (s[i])
+                        {
+                            case '1':
+                                ones++;
+                                break;
+                            case '0':
+                                zeros++;
+                                break;
+
+                        }
+                        break;
+                }
+            }
+            return Math.Min(ones, zeros);
+        }
+    }
+}
