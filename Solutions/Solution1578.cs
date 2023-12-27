26cde2fbffaea139ec248756cfe9d48e0273fcd8
kasla <kasla@DESKTOP-SR937O1>
2023-12-26 21:41:34 -0800

    1578

---
+++ Solutions/Solution1578.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode.Solutions
+{
+    public class Solution1578
+    {
+        public int MinCost(string colors, int[] neededTime)
+        {
+            char previousColor = ' ';
+            int previousTime = 0;
+            int currentCost = 0;
+            int previousCost = 0;
+
+            for (int i = 0; i < colors.Length; i++)
+            {
+                if (colors[i] == previousColor)
+                {
+                    int currentMinTime = Math.Min(neededTime[i], previousTime);
+                    currentCost = currentMinTime + previousCost;
+                    previousTime = Math.Max(neededTime[i], previousTime);
+                }
+                else
+                {
+                    currentCost = previousCost;
+                    previousColor = colors[i];
+                    previousTime = neededTime[i];
+                }
+                previousCost = currentCost;
+            }
+
+            return previousCost;
+        }
+    }
+}
