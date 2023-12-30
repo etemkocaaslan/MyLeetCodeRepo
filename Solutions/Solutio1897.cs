dd8f4a6e758b75e0a53e8db3f37d9849228b1bc2
kasla <kasla@DESKTOP-SR937O1>
2023-12-30 09:01:20 -0800

    Q 1897, 1335 have solved

---
+++ Solutions/Solutio1897.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode.Solutions
+{
+    public class Solution1897
+    {
+        public bool MakeEqual(string[] words)
+        {
+            Dictionary<char, int> map = new();
+
+            foreach (string word in words)
+            {
+                foreach (char c in word)
+                {
+                    if (map.ContainsKey(c))
+                    {
+                        map[c]++;
+                    }
+                    else
+                    {
+                        map[c] = 1;
+                    }
+                }
+            }
+
+            foreach (int count in map.Values)
+            {
+                if (count % words.Length != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- Solutions/Solution1335.cs
+++ Solutions/Solution1335.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace leetcode.Solutions
+﻿namespace leetcode.Solutions
 {
     public class Solution1335
     {
