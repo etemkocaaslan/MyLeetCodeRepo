c7473fb7a05fefd0f0fb24c350fc8819352f692a
kasla <kasla@DESKTOP-SR937O1>
2024-01-02 23:45:48 -0800

    2610

---
+++ Solutions/Solution2610.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode.Solutions
+{
+    public class Solution
+    {
+        public IList<IList<int>> FindMatrix(int[] nums)
+        {
+            List<int> container = new();
+            List<int> matrix = nums.ToList();
+
+            IList<IList<int>> list = new List<IList<int>>();
+            while (matrix.Count > 0)
+            {
+                for (int i = 0; i < matrix.Count; i++)
+                {
+                    if (container.Count == 0 || !container.Contains(matrix[i]))
+                    {
+                        container.Add(matrix[i]);
+                        matrix.Remove(matrix[i]);
+                        i--;
+                    }
+
+                }
+                list.Add(container.ToArray());
+                container.Clear();
+            }
+            return list;
+        }
+    }
+}
