ef3d8297a733dfa4463bf11c4241bc470bcdd0ea
kasla <kasla@DESKTOP-SR937O1>
2024-01-04 02:41:51 -0800

    217

---
+++ Solutions/Solution217.cs
@@ -0,0 +1,10 @@
+﻿namespace leetcode.Solutions
+{
+    public class Solution217
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            return nums.Length != nums.Distinct().Count();
+        }
+    }
+}
