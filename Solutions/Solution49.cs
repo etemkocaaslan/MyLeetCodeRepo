6c234574e649aa393ad76b0f9c22edfab0c77c5e
kasla <kasla@DESKTOP-SR937O1>
2024-01-04 14:02:51 -0800

    49

---
+++ Solutions/Solution49.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode.Solutions
+{
+    public class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, IList<string>> dict = new();
+
+            for (int i = 0; i < strs.Length; i++)
+            {
+                char[] arr = strs[i].ToCharArray();
+                Array.Sort(arr);
+
+                if (!dict.TryGetValue(new String(arr), out IList<string>? value))
+                    dict.Add(new String(arr), new List<String>() { strs[i] });
+                else
+                    value.Add(strs[i]);
+            }
+
+            return dict.Values.ToList();
+        }
+    }
+}
