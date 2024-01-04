116e4b9e928817bde61c2b8a41533dbaa4b1f717
kasla <kasla@DESKTOP-SR937O1>
2024-01-03 17:57:36 -0800

    2125

---
+++ Solutions/Solution2125.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode.Solutions
+{
+    public class Solution2125
+    {
+        public int NumberOfBeams(string[] bank)
+        {
+            int result = 0;
+            List<int> security = new();
+            for (int i = 0; i < bank.Length; ++i)
+            {
+                int curr = bank[i].Count(x => x == '1');
+                if (curr != 0)
+                {
+                    security.Add(curr);
+                }
+                if (security.Count == 2)
+                {
+                    result += security[0] * security[1];
+                    security[0] = security[1];
+                    security.RemoveAt(0);
+                }
+            }
+            return result;
+        }
+    }
+    public class Solution2125_2
+    {
+        public int NumberOfBeams(string[] bank)
+        {
+            int[] numOfCam = bank.Select(r => r.Count(d => d == '1')).Where(cnt => cnt > 0).ToArray();
+            int result = 0;
+            for (int i = 1; i < numOfCam.Length; ++i) 
+                result += numOfCam[i] * numOfCam[i - 1];
+            return result;
+        }
+    }
+}
+
